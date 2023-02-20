5ab0bf560820ca4ff422889d41b99b939df02028
Ungku Amer Ungku Aziz <ungku@Ungkus-MacBook-Air.local>
2023-02-20 23:58:23 +0000

    Completed Track class and FullTests passed

--- src/main/java/Track.java
+++ src/main/java/Track.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.nio.file.Path;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * Represents a point in space and time, recorded by a GPS sensor.
@@ -76,11 +74,74 @@ public class Track {
     }
     // TODO: Create a stub for lowestPoint()
 
+    public Point lowestPoint() {
+
+        if (trackPoints.size() == 0) {
+            throw new GPSException("Track is empty");
+        }
+        Collections.sort(trackPoints, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return Double.compare(o1.getElevation(), o2.getElevation());
+            }
+        });
+
+        return trackPoints.get(0);
+    }
 
     // TODO: Create a stub for highestPoint()
 
+    public Point highestPoint () {
+
+        if (trackPoints.size() == 0) {
+            throw new GPSException("Track is empty");
+        }
+        Collections.sort(trackPoints, new Comparator<Point>() {
+            @Override
+            public int compare(Point o1, Point o2) {
+                return Double.compare(o1.getElevation(), o2.getElevation());
+            }
+        });
+
+        int getFrom = trackPoints.size()-1;
+        return trackPoints.get(getFrom);
+    }
     // TODO: Create a stub for totalDistance()
+    public Double totalDistance() {
 
+        if (trackPoints.size() < 2) {
+            throw new GPSException("Not enough tracks");
+        }
+
+        double totalD = 0;
+
+        for (int j = 0; j < trackPoints.size()-1; j++) {
+            Point currentPoint = trackPoints.get(j);
+            Point nextPoint = trackPoints.get(j+1);
+
+            double distance = Point.greatCircleDistance(currentPoint, nextPoint);
+            totalD += distance;
+        }
+        return totalD;
+    }
     // TODO: Create a stub for averageSpeed()
+    public Double averageSpeed() {
 
+        if (trackPoints.size() < 2) {
+            throw new GPSException("Not enough tracks");
+        }
+
+        double totalTime = 0;
+
+        for (int k = 0; k < trackPoints.size()-1; k++) {
+            ZonedDateTime currentPoint = trackPoints.get(k).getTime();
+            ZonedDateTime nextPoint = trackPoints.get(k+1).getTime();
+
+            double currentTime = SECONDS.between(currentPoint, nextPoint);
+            totalTime += currentTime;
+        }
+
+        double avgSpeed = totalDistance()/totalTime;
+        return avgSpeed;
+    }
 }
